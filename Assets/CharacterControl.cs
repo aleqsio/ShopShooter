68d21fcbfd52f7928d601bf8ccac1fb16609610e
Aleksander Mikucki <aleksander.mikucki@swmansion.com>
2020-07-27 00:12:41 +0200

    add first version

---
+++ Assets/CharacterControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CharacterControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Speed;
+    public float Gravity;
+    private CharacterController _controller;
+    private Vector3 _velocity;
+    public GameObject rig;
+    private Animator _animator;
+    private bool _isGrounded;
+    private Vector3 _lastPosition;
+    public float gravityDelay;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _animator = rig.GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(move * Time.deltaTime * Speed);
+        _animator.SetFloat("MoveSpeed", move.magnitude);
+        if (move != Vector3.zero)
+            transform.forward = move;
+        if(_lastPosition.y <= this.transform.position.y){
+            _animator.SetBool("Grounded", true);
+            _velocity.y = 0f;
+        }else{
+            Debug.Log('x'+Mathf.Abs(_velocity.y)+'y' + (Gravity * Time.deltaTime * 1000));
+            _animator.SetBool("Grounded", Mathf.Abs(_velocity.y) < Mathf.Abs(Gravity * Time.deltaTime * gravityDelay));
+        }
+        
+        _velocity.y += Gravity * Time.deltaTime;
+        _lastPosition = this.transform.position;
+        _controller.Move(_velocity * Time.deltaTime);
+        
+    }
+
+}
---
+++ Assets/SpawnPaper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPaper : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(count>0){
+            count--;
+            if(count%2==0){
+
+            
+            Instantiate(myPrefab, this.transform.position, Quaternion.identity);
+            }
+        }
+
+    }
+}
